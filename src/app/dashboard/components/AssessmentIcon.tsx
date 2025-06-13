d7ea24dcda646507f200c01ac4cc6a356bc4323f
miyaparker <karen@cchub.africa>
2025-06-13 12:06:58 +0100

    remove loca; src/app folder to add submodule

--- src/app/dashboard/components/AssessmentIcon.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-
-const AssessmentIcon: React.FC = () => {
-  return <AssignmentIcon />;
-};
-
-export default AssessmentIcon; 
