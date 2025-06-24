bca8efed3b813a8dfdd2ced484bf6a557c51cf04
miyaparker <karen@cchub.africa>
2025-06-24 09:26:53 +0100

    chore: update shared app module to latest commit

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,7 +1,10 @@
 export default {
-    title: 'ElevateHR',
-    description: 'ElevateHR - AI-Powered Recruitment Platform',
-    icons: {
-      icon: '/favicon.svg',
-    },
-  } 
+  host:"ElevateHR",
+  logo:"/images/logos/logo-light.svg",
+  favicon:"/images/logos/favicon.svg",
+  title:"ElevateHR.ng",
+  description:"ElevateHR.ng is a platform for assessing candidates for jobs.",
+  keywords:"ElevateHR.ng,elevatehr.ng, elevatehrng, elevatehrng.com, elevatehrng.com",
+  author:"ElevateHR.ng",
+  defaultBookingLink:"https://app.elevatehr.ng/booking",
+}
